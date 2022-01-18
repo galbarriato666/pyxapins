f802b4c79165d545cddfd6d0085153d6ba87828d
sjo <emmanuel.manzanilla.7e5@itb.cat>
2022-01-18 13:04:01 +0100

    practica

---
+++ M03 UF1/Llistes/Pr5 - Tipus de Dades :  Ex1.py
@@ -0,0 +1,43 @@
+
+"""1. Programa de càlcul de temperatures emmagatzemades dins d’una llista de nombres sencers (sense decimals).
+Proveu el resultat usant les definicions següents d’array, una per a cada execució diferent:
+//Primer proveu si funciona usant aquesta llista"""
+
+
+
+
+
+
+gener = [1, 12, 4, -5, 7, 3, 2,
+        9, -6, 7, 8, 2, 14, -7,
+        5, 13, 12, 4, 6, -7, 1,
+        12, 4, -2, 3, 5, 2, 7,
+        -2, 8,-15]
+
+max_temp = 1
+min_temp = 1
+
+"""Cal definir els 3 primers mesos de l’any, i calcular:
+Per a cada mes: temperatura màxima, mínima i mitjana
+Per al 1r trimestre de l’any: temperatura màxima, mínima i mitjana"""
+
+#por cada vuelta en enero
+    #si cada vuelta es mayor que cada vuelta
+    #max_temp.append(cada vuelta)
+
+
+
+for temp in gener:
+    if (max_temp == 1 or temp > max_temp):
+        print(temp)
+
+
+
+
+
+
+
+
+
+
+
