a158df46487cb0b7bf925e9cbe14a9cf53de47a2
galbarriato666 <emmanuel.manzanilla.7e5@itb.cat>
2022-05-05 09:51:31 +0200

    oaoaoa

--- JAVASCRIPT/hola.js
+++ JAVASCRIPT/hola.js
@@ -1,1 +1,16 @@
-console.log("Coco")
+//Prendre un número aleatori entre 1 i 20, i demanar amb una notificació a l’usuari que l’endevini.
+//Aquest exercici és per treballar condicionals, funcions matemàtiques, 
+//i funcions d’interacció amb el navegador i usuari (prompt, console.log)
+
+//DOM EXERCICI 3//
+
+boton = document.getElementById("1")
+boton.addEventListener("click", myScript); 
+
+function myScript(){
+  parrafos = document.getElementsByTagName("p");
+  for(let x=0; x<parrafos.length; x++){
+  console.log(parrafos[x]);
+    parrafos[x].style.backgroundColor = "green";
+  }
+}
