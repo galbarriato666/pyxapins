68bc141177e8dde977ab109dcf91bfd02dc9f240
sjo <dada>
2022-05-17 10:37:52 +0200

    ex3

--- UF3/nomsUF3/Ejercicio 3/Ejercicio 3.py
+++ UF3/nomsUF3/Ejercicio 3/Ejercicio 3.py
@@ -1,7 +1,7 @@
 '''
-Miquel Regué Barrera
+Emmanuel Manzanilla García
 17/05/2022
-ASIXc M03 UF2 A1 Disseny Descendent
+ASIXb M03 UF2 A1 Disseny Descendent
 
 Realizar un programa que a partir del archivo, "ocells.txt" cree otro
 "ocells_2.txt". el contenido de cada fichero debe ser:
@@ -11,35 +11,35 @@ Foto 1
 Foto 2
 '''
 
-ruta_entrada = "./ocells.txt"
-ruta_salida = "./ocells_2.txt"
+inbound = "./ocells.txt"
+outbound = "./ocells_2.txt"
 
-def LeeryEscribirUnArchivo(input, output):
+def ReadWrite(input, output):
     """
     Lee un archivo y lo copia en otro añadiendo contenido
     :param input: Ruta de entrada del archivo
     :param output: Ruta de salida del archivo
     """
-    contador = 0
+    comptador = 0
     try:
-        with open(input, "rt") as archivo:
-            with open(output, "wt") as archivo_escrito:
-                for lineas in archivo:
-                    for caracteres in lineas:
-                        if caracteres == "6":
-                            contador += 1
-                        if contador == 2:
-                                archivo_escrito.write("*")
-                                contador += 1
-                        elif caracteres == "3":
-                            archivo_escrito.write("-")
+        with open(input, "rt") as file:
+            with open(output, "wt") as outputFile:
+                for linies in file:
+                    for char in linies:
+                        if char == "6":
+                            comptador += 1
+                        if comptador == 2:
+                                outputFile.write("*")
+                                comptador += 1
+                        elif char == "3":
+                            outputFile.write("-")
                         else:
-                            archivo_escrito.write(caracteres)
+                            outputFile.write(char)
 
 
 
-    except UnicodeError or OSError as mensaje:
-        print(mensaje)
+    except UnicodeError or OSError as ErrorCode:
+        print(ErrorCode)
 
 
-LeeryEscribirUnArchivo(ruta_entrada, ruta_salida)
+CallReadWriteF(inbound, outbound)
