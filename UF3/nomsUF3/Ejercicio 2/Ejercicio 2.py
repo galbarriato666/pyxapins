57c560abc54c8e023a442e9c1f16d7701c1584fb
sjo <dada>
2022-05-17 09:42:06 +0200

    ex2

--- UF3/nomsUF3/Ejercicio 2/Ejercicio 2.py
+++ UF3/nomsUF3/Ejercicio 2/Ejercicio 2.py
@@ -1,11 +1,11 @@
 '''
-Miquel Regué Barrera
+Emmanuel Manzanilla García
 17/05/2022
-ASIXc M03 UF2 A1 Disseny Descendent
+ASIXb M03 UF2 A1 Disseny Descendent
 
 Crea una copia del archivo "paraules.txt" añadiendo un número que indique
 la cantidad de caractéres de cada una de las palabras del fichero.
-El fichero resultado se denominará "paraulesQuantat.txt"
+El fichero resultado se denominará "paraulesQuantitat.txt"
 
 El formato de cada linea debe ser: "cantidad + espacio + palabra"
 ej.
@@ -15,27 +15,26 @@ ej.
 9 absorbats
 '''
 
-ruta_entrada = "./paraules.txt"
-ruta_salida = "./paraparaulesQuantat.txt"
+inbound = "./paraules.txt"
+outbound = "./paraulesQuantitat.txt"
 
-def LeeryEscribirUnArchivo(input, output):
+def ReadWrite(input, output):
     """
     Lee un archivo y lo copia en otro añadiendo contenido
     :param input: Ruta de entrada del archivo
     :param output: Ruta de salida del archivo
     """
-    lista_palabras = []
+    wordlist = []
     try:
-        with open(input, "rt") as archivo:
-            for palabras in archivo:
-                palabras_sin_salto =  palabras.strip("\n")
-                lista_palabras.append(palabras_sin_salto)
+        with open(input, "rt") as file:
+            for palabras in file:
+                palabrasNoLinea =  palabras.strip("\n")
+                wordlist.append(palabrasNoLinea)
 
-            with open(output, "wt") as archivo_escrito:
-                for i in range(len(lista_palabras)):
-                    archivo_escrito.write(f'{len(lista_palabras[i])} {lista_palabras[i]} \n')
-    except UnicodeError or OSError as mensaje:
-        print(mensaje)
+            with open(output, "wt") as outboundFile:
+                for i in range(len(wordlist)):
+                    outboundFile.write(f'{len(wordlist[i])} {wordlist[i]} \n')
+    except UnicodeError or OSError as ErrorCode:
+        print(ErrorCode)
 
-
-LeeryEscribirUnArchivo(ruta_entrada, ruta_salida)
+ReadWrite(inbound, outbound)
